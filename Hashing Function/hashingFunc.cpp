24f284227127a7689ea66d1bec818950e528f5c9
adi105 <adrian_bernat@yahoo.com>
2017-05-22 23:54:29 -0700

    Started hashing function

---
+++ Hashing Function/hashingFunc.cpp
@@ -0,0 +1,20 @@
+//******************************************************************************
+// Program Name:	hashingFunc.cpp
+// Developer:		Adrian Bernat
+// Course Number:	CS-260
+// Date:			5-21-17
+// IDE:				Visual Studio 2015
+// Description:		Contains the code that implements a hashing function and tests
+//					it out by inserting items from a list of 45,402 words.
+//*******************************************************************************
+
+#include <iostream>
+using namespace std;
+
+//prototypes
+void hashFunction();
+
+int main() {
+
+	return 0;
+}

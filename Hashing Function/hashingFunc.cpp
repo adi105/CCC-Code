94f71cce0d1cb8a40c4faa57551e713b6e43837f
adi105 <adrian_bernat@yahoo.com>
2017-05-23 23:11:28 -0700

    started hash

--- Hashing Function/hashingFunc.cpp
+++ Hashing Function/hashingFunc.cpp
@@ -9,12 +9,83 @@
 //*******************************************************************************
 
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
+const int NUMBER_OF_WORDS = 45402;
+const int DATA_SIZE = 40;
+
+const string FILE_TO_READ = "words.txt";
+const string FILE_TO_WRITE = "hashresults.csv";
+
+ifstream fileRead;
+ofstream fileWrite;
+
 //prototypes
-void hashFunction();
+int hashFunc(string wordToHash);
+float findCollision(const int* arrayToSearch);
+void outputToFile(const int* arrayToOutput);
+string convertToLowercase(string wordToConvert);
+
 
 int main() {
+	//declare variables
+	int hashMap[NUMBER_OF_WORDS] = {};
+	char wordFromLine[DATA_SIZE] = {};
+	string wordFromLineString = "";
+	int arrayIndex = 0;
+	int errorCount = 0;
+	
+	fileRead.open(FILE_TO_READ);
+
+	//check if it opened
+	if (!fileRead.is_open()) {
+		cout << "ERROR! Cannot open file." << endl;
+		return;
+	}
+
+	else {
+		//go through the file
+		while (!fileRead.eof()) {
+			fileRead.getline(wordFromLine, DATA_SIZE);
+
+			//convert to a string
+			wordFromLineString = wordFromLine;
+			wordFromLineString = convertToLowercase(wordFromLineString);
+
+			//calculate the index
+			arrayIndex = hashFunc(wordFromLineString) % NUMBER_OF_WORDS;
+			arrayIndex = abs(arrayIndex);
+
+			if (arrayIndex < 0 || arrayIndex >= NUMBER_OF_WORDS) {
+				cout << "ERROR! Index out of bounds" << endl;
+				errorCount++;
+				continue;
+			}
+
+			hashMap[arrayIndex]++;
+		}
+
+		fileRead.close();
+
+		cout << "CS-260: A6 Hashing, Adrian Bernat, " << findCollision(hashMap) << "% error " << errorCount << endl;
+	}
 
 	return 0;
 }
+
+//========================================================================
+string convertToLowercase(string wordToConvert) {
+	for (int index = 0; index < wordToConvert.length(); index++) {
+		if (wordToConvert[index] >= 65 && wordToConvert[index] <= 90) {
+			wordToConvert[index] = tolower(wordToConvert[index]);
+		}
+		else {
+			continue;
+		}
+	}
+	return wordToConvert;
+}
+
+//=========================================================================
